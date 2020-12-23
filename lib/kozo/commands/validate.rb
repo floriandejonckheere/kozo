7bf1bd6b7e6ef31c96340c8cc1992aa2725b729f
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-12-23 13:19:52 +0100

    Add validate command

---
+++ lib/kozo/commands/validate.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Kozo
+  module Commands
+    class Validate < Base
+      self.description = "Check whether configuration is valid"
+
+      def start
+        configuration.parse!
+
+        Kozo.logger.info "the configuration is valid"
+      end
+    end
+  end
+end
