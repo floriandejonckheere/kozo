5dd3165e4e12556d6740c0d61b2c144ae7f968ed
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-12-30 11:19:44 +0100

    Initialize providers after build

--- spec/factories/providers.rb
+++ spec/factories/providers.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :provider, class: "Kozo::Provider"
+  factory :provider, class: "Kozo::Provider" do
+    after(:build, &:initialize!)
+  end
 end
