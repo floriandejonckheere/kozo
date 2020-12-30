708caf620967241c805e05b359df4abd8aab2912
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-12-30 13:10:39 +0100

    Add Provider#==

--- lib/kozo/provider.rb
+++ lib/kozo/provider.rb
@@ -3,5 +3,9 @@
 module Kozo
   class Provider
     class_attribute :provider_name
+
+    def ==(other)
+      self.class.provider_name == other.class.provider_name
+    end
   end
 end
--- spec/kozo/provider_spec.rb
+++ spec/kozo/provider_spec.rb
@@ -3,7 +3,19 @@
 RSpec.describe Kozo::Provider do
   subject(:provider) { described_class.new }
 
-  it "has a name" do
-    expect(described_class).to respond_to :provider_name, :provider_name=
+  describe "class methods" do
+    subject { described_class }
+
+    it { is_expected.to respond_to :provider_name, :provider_name= }
+  end
+
+  describe "#==" do
+    subject(:provider) { provider_one_class.new }
+
+    let(:provider_one_class) { Class.new(described_class) { self.provider_name = "provider_one" } }
+    let(:provider_two_class) { Class.new(described_class) { self.provider_name = "provider_two" } }
+
+    it { is_expected.to eq provider_one_class.new }
+    it { is_expected.not_to eq provider_two_class.new }
   end
 end
