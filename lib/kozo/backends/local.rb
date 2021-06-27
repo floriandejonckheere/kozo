38096a993ca830caf07d38f19e5e06681f95d596
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-06-27 20:15:04 +0200

    Set state, not data

--- lib/kozo/backends/local.rb
+++ lib/kozo/backends/local.rb
@@ -16,7 +16,7 @@ module Kozo
       def initialize!
         Kozo.logger.debug "Initializing local state in #{path}"
 
-        self.data = State.new.to_h unless File.exist?(path)
+        self.state = State.new unless File.exist?(path)
       end
 
       def data
