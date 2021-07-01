a6e322c9959c316c534ab0c21e919b60315d6f9e
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-07-01 18:25:14 +0200

    Discard other logs in log matcher

--- spec/support/logger.rb
+++ spec/support/logger.rb
@@ -4,13 +4,22 @@ RSpec::Matchers.define :log do |expected|
   match do |actual|
     logger = double
 
+    # Mock Kozo.logger
     allow(Kozo)
       .to receive(:logger)
       .and_return logger
 
+    # Store logs with level 'info'
     allow(logger)
       .to receive(:info) { |s| (@messages ||= []) << s }
       .and_return nil
+
+    # Discard logs with other levels
+    [:error, :warn, :debug].each do |level|
+      allow(logger)
+        .to receive(level)
+        .and_return nil
+    end
 
     actual.call
 
@@ -21,10 +30,9 @@ RSpec::Matchers.define :log do |expected|
   failure_message do |_actual|
     messages = @messages
       &.map
-      &.with_index { |m, i| m.prepend("#{(i + 1).to_s.rjust(5)}) ") }
-      &.join("\n")
+      &.with_index { |m, i| m.prepend("\n#{(i + 1).to_s.rjust(5)}) ") } || "nothing"
 
-    "expected block to log #{expected}, but received:\n#{messages}"
+    "expected block to log #{expected}, but received #{messages}"
   end
 
   supports_block_expectations
