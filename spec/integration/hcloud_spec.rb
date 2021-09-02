8c1164b8b5897621c31dd62401c66279c490a50b
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-09-02 16:06:45 +0200

    --wip-- [skip ci]

--- spec/integration/hcloud_spec.rb
+++ spec/integration/hcloud_spec.rb
@@ -1,17 +1,29 @@
 # frozen_string_literal: true
 
+# require "fakefs/spec_helpers"
+require "fileutils"
+
 RSpec.describe "Hetzner Cloud", integration: true, order: :defined do
-  include FakeFS::SpecHelpers
+  # include FakeFS::SpecHelpers
 
   let(:directory) { Dir.mktmpdir }
 
-  around do |example|
-    FakeFS do
-      FakeFS::FileSystem.clone(Kozo.root.join("spec/support/fixtures/hcloud"))
+  before { FileUtils.cp_r Kozo.root.join("spec/fixtures/hcloud/."), directory }
 
-      example.run
-    end
+  around { |example| Dir.chdir(directory) { example.run } }
+
+  after { FileUtils.rm_rf directory }
+
+  # around do |example|
+  #   FakeFS do
+  #     FakeFS::FileSystem.clone(Kozo.root.join("spec/fixtures/hcloud").to_s)
+  #
+  #     example.run
+  #   end
+  # end
+
+  it "creates, updates and destroys resources in Hetzner Cloud" do
+    require "byebug"
+    byebug
   end
-
-  xit "creates, updates and destroys resources in Hetzner Cloud"
 end
