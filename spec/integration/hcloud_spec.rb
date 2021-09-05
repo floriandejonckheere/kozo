899eea14e00fffdf4d37cffbf289d6a766caf21c
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-09-05 12:04:53 +0200

    Add integration test

--- spec/integration/hcloud_spec.rb
+++ spec/integration/hcloud_spec.rb
@@ -1,17 +1,42 @@
 # frozen_string_literal: true
 
+require "English"
+require "fileutils"
+
+# rubocop:disable RSpec/BeforeAfterAll, RSpec/InstanceVariable
 RSpec.describe "Hetzner Cloud", integration: true, order: :defined do
-  include FakeFS::SpecHelpers
+  before(:all) do |_example|
+    @directory = Dir.mktmpdir
 
-  let(:directory) { Dir.mktmpdir }
-
-  around do |example|
-    FakeFS do
-      FakeFS::FileSystem.clone(Kozo.root.join("spec/support/fixtures/hcloud"))
-
-      example.run
-    end
+    # Set up working directory (shared files)
+    FileUtils.cp_r Kozo.root.join("spec/fixtures/hcloud/shared/."), @directory
   end
 
-  xit "creates, updates and destroys resources in Hetzner Cloud"
+  after(:all) do |_example|
+    # Tear down working directory
+    FileUtils.rm_r @directory
+  end
+
+  before do |example|
+    # Set up working directory (fixtures)
+    FileUtils.cp_r Kozo.root.join("spec/fixtures/hcloud/#{example.metadata[:fixture]}/."), @directory if example.metadata[:fixture]
+  end
+
+  around do |example|
+    Dir.chdir(@directory) { example.run }
+  end
+
+  it "initializes state" do
+    `kozo init`
+
+    expect(File).to exist File.join(@directory, "kozo.kzstate")
+  end
+
+  it "plans resource creation", fixture: "ssh_key" do
+    output = `kozo plan`
+
+    expect($CHILD_STATUS.exitstatus).to be_zero
+    expect(output.uncolorize).to include "+ hcloud_ssh_key.default"
+  end
 end
+# rubocop:enable RSpec/BeforeAfterAll, RSpec/InstanceVariable
