810b8335074c5f2f25cfc85b8087acfa9dee36a9
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-09-09 17:03:16 +0200

    Update integration spec

--- spec/integration/hcloud_spec.rb
+++ spec/integration/hcloud_spec.rb
@@ -10,11 +10,17 @@ RSpec.describe "Hetzner Cloud", integration: true, order: :defined do
 
     # Set up working directory (shared files)
     FileUtils.cp_r Kozo.root.join("spec/fixtures/hcloud/shared/."), @directory
+
+    # Set HCloud client
+    HCloud::Client.connection = HCloud::Client.new(access_token: ENV.fetch("HCLOUD_TOKEN"))
   end
 
   after(:all) do |_example|
     # Tear down working directory
     FileUtils.rm_r @directory
+
+    # Delete resources
+    HCloud::SSHKey.all.each(&:delete)
   end
 
   before do |example|
@@ -32,11 +38,40 @@ RSpec.describe "Hetzner Cloud", integration: true, order: :defined do
     expect(File).to exist File.join(@directory, "kozo.kzstate")
   end
 
-  it "plans resource creation", fixture: "ssh_key" do
+  it "creates an SSH key", fixture: "ssh_key_one" do
     output = `kozo plan`
 
     expect($CHILD_STATUS.exitstatus).to be_zero
     expect(output.uncolorize).to include "+ hcloud_ssh_key.default"
+
+    `kozo apply --yes`
+
+    expect($CHILD_STATUS.exitstatus).to be_zero
+    expect(HCloud::SSHKey.all.first.name).to eq "default"
+  end
+
+  it "updates an SSH key", fixture: "ssh_key_two" do
+    output = `kozo plan`
+
+    expect($CHILD_STATUS.exitstatus).to be_zero
+    expect(output.uncolorize).to include "~ hcloud_ssh_key.default"
+
+    `kozo apply --yes`
+
+    expect($CHILD_STATUS.exitstatus).to be_zero
+    expect(HCloud::SSHKey.all.first.name).to eq "new_default"
+  end
+
+  it "removes an SSH key", fixture: "ssh_key_three" do
+    output = `kozo plan`
+
+    expect($CHILD_STATUS.exitstatus).to be_zero
+    expect(output.uncolorize).to include "- hcloud_ssh_key.default"
+
+    `kozo apply --yes`
+
+    expect($CHILD_STATUS.exitstatus).to be_zero
+    expect(HCloud::SSHKey.all).to be_empty
   end
 end
 # rubocop:enable RSpec/BeforeAfterAll, RSpec/InstanceVariable
