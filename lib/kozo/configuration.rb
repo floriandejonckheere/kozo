b36437fefdba5565a068e469b416a0ee738bfa72
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-09-16 19:13:43 +0200

    Merge environment into configuration

--- lib/kozo/configuration.rb
+++ lib/kozo/configuration.rb
@@ -17,6 +17,34 @@ module Kozo
         .resolve("backend.local", self, directory)
     end
 
+    def changes
+      @changes ||= begin
+        # Copy resources in state
+        changes = backend
+          .state
+          .resources
+          .map(&:dup)
+          .each(&:changes_applied)
+          .each do |resource|
+          configured = resources.find { |r| r.address == resource.address }
+
+          # Assign updated attributes (mark for update)
+          resource.assign_attributes(configured&.attributes&.except(:id) || resource.attributes.transform_values { nil })
+
+          # Mark for deletion
+          resource.mark_for_deletion! unless configured
+        end
+
+        # Append resources not in state
+        changes += resources
+          .reject { |r| backend.state.resources.any? { |res| res.address == r.address } }
+          .map { |r| r.class.new(r.attributes) }
+          .each(&:mark_for_creation!)
+
+        changes
+      end
+    end
+
     def to_s
       "directory: #{directory}"
     end
--- spec/kozo/configuration_spec.rb
+++ spec/kozo/configuration_spec.rb
@@ -1,9 +1,57 @@
 # frozen_string_literal: true
 
 RSpec.describe Kozo::Configuration do
-  subject(:configuration) { build(:configuration, directory: Dir.pwd) }
+  subject(:configuration) { build(:configuration) }
 
   it "defaults to local backend" do
     expect(configuration.backend).to be_a Kozo::Backends::Local
+  end
+
+  describe "#changes" do
+    subject(:configuration) { build(:configuration, resources: resources, backend: build(:local_backend, state: build(:state, resources: state))) }
+
+    let(:state) { [] }
+    let(:resources) { [] }
+
+    context "when resources have to be created" do
+      let(:state) { [] }
+      let(:resources) { [build(:dummy_resource, id: nil, state_name: "state_name", name: "name", description: "description")] }
+
+      it "marks resource for creation" do
+        expect(configuration.changes.count).to eq 1
+
+        resource = configuration.changes.first
+
+        expect(resource).to be_marked_for_creation
+        expect(resource.changes.symbolize_keys).to eq name: [nil, "name"], description: [nil, "description"]
+      end
+    end
+
+    context "when resources have to be updated" do
+      let(:state) { [build(:dummy_resource, id: "id", state_name: "state_name", name: "name", description: "description")] }
+      let(:resources) { [build(:dummy_resource, id: nil, state_name: "state_name", name: "new name", description: "new description")] }
+
+      it "marks resource for update" do
+        expect(configuration.changes.count).to eq 1
+
+        resource = configuration.changes.first
+
+        expect(resource.changes.symbolize_keys).to eq name: ["name", "new name"], description: ["description", "new description"]
+      end
+    end
+
+    context "when resources have to be deleted" do
+      let(:state) { [build(:dummy_resource, id: "id", state_name: "state_name", name: "name", description: "description")] }
+      let(:resources) { [] }
+
+      it "marks resource for deletion" do
+        expect(configuration.changes.count).to eq 1
+
+        resource = configuration.changes.first
+
+        expect(resource).to be_marked_for_deletion
+        expect(resource.changes.symbolize_keys).to eq id: ["id", nil], name: ["name", nil], description: ["description", nil]
+      end
+    end
   end
 end
