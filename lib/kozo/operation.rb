4ff199bd7f7727a660b88bbc24a610c9cf91af62
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-10-14 19:25:00 +0200

    Render attribute differences in execution plan

--- lib/kozo/operation.rb
+++ lib/kozo/operation.rb
@@ -15,7 +15,16 @@ module Kozo
     end
 
     def to_s
-      "#{display_symbol} #{resource.address}"
+      # Max attribute name length
+      l = resource.attribute_names.map(&:length).max || 1
+
+      <<~DSL.chomp
+        #{"# #{resource.address}:".bold}
+        #{display_symbol} resource "#{resource.resource_name}", "#{resource.state_name}" do |r|
+          #{resource.attributes.map { |k, v| "#{resource.changes.key?(k) ? display_symbol : ' '}  r.#{k.to_s.ljust(l)} = \"#{v.to_s.chomp.truncate(75)}\"" }.join("\n  ")}
+        end
+
+      DSL
     end
   end
 end
--- spec/kozo/commands/plan_spec.rb
+++ spec/kozo/commands/plan_spec.rb
@@ -3,26 +3,46 @@
 RSpec.describe Kozo::Commands::Plan do
   subject(:command) { build(:plan_command, configuration: configuration) }
 
-  let(:resource0) { build(:dummy_resource, state_name: "resource0", name: "resource0") }
-  let(:resource1) { build(:dummy_resource, state_name: "resource1", name: "resource1") }
-  let(:resource2) { build(:dummy_resource, state_name: "resource2", name: "resource2") }
+  let(:resource0) { build(:dummy_resource, state_name: "resource0", name: "name0", description: "description0", id: nil) }
+  let(:resource1) { build(:dummy_resource, state_name: "resource1", name: "name1", description: "description1") }
+  let(:resource2) { build(:dummy_resource, state_name: "resource2", name: "name2", description: "description2") }
 
-  let(:resource1_modified) { build(:dummy_resource, state_name: "resource1", name: "old name") }
+  let(:resource1_modified) { resource1.dup.tap { |r| r.name = "old name1" } }
 
   let(:state) { build(:state, resources: [resource1_modified, resource2]) }
-  let(:backend) { build(:memory_backend, state: state) }
-  let(:configuration) { build(:configuration, backend: backend, resources: [resource0, resource1]) }
+  let(:configuration) { build(:configuration, backend: build(:memory_backend, state: state), resources: [resource0, resource1]) }
 
   it "plans an addition of a resource" do
-    expect { command.start }.to log("+ dummy.resource0")
+    expect { command.start }.to log <<~LOG.chomp
+      # dummy.resource0:
+      + resource "dummy", "resource0" do |r|
+           r.id          = ""
+        +  r.name        = "name0"
+        +  r.description = "description0"
+      end
+    LOG
   end
 
   it "plans a modification of a resource" do
-    expect { command.start }.to log("~ dummy.resource1")
+    expect { command.start }.to log <<~LOG.chomp
+      # dummy.resource1:
+      ~ resource "dummy", "resource1" do |r|
+           r.id          = "#{resource1.id}"
+        ~  r.name        = "name1"
+           r.description = "description1"
+      end
+    LOG
   end
 
   it "plans a deletion of a resource" do
-    expect { command.start }.to log("- dummy.resource2")
+    expect { command.start }.to log <<~LOG.chomp
+      # dummy.resource2:
+      - resource "dummy", "resource2" do |r|
+        -  r.id          = ""
+        -  r.name        = ""
+        -  r.description = ""
+      end
+    LOG
   end
 
   context "when nothing can be done" do
