20c0846758c5613b43edf27cdd7bd86abfac0526
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-10-17 11:40:09 +0200

    Extract CRUD logic to HCloud resource

--- lib/kozo/providers/hcloud/resource.rb
+++ lib/kozo/providers/hcloud/resource.rb
@@ -7,6 +7,49 @@ module Kozo
         self.provider_name = "hcloud"
 
         attribute :id, :big_integer
+
+        protected
+
+        def refresh
+          resource = resource_class.find(id)
+
+          attribute_names
+            .excluding(:id)
+            .each { |attr| send(:"#{attr}=", resource.send(attr)) }
+        end
+
+        def create
+          resource = resource_class.new(**attributes.except(:id))
+          resource.create
+
+          attribute_names
+            .each { |attr| send(:"#{attr}=", resource.send(attr)) }
+        end
+
+        def update
+          resource = resource_class.find(id)
+
+          attribute_names
+            .excluding(:id)
+            .each { |attr| resource.send(:"#{attr}=", send(attr)) }
+
+          resource.update
+
+          attribute_names
+            .excluding(:id)
+            .each { |attr| send(:"#{attr}=", resource.send(attr)) }
+        end
+
+        def destroy
+          resource = resource_class.find(id)
+          resource.delete
+        end
+
+        private
+
+        def resource_class
+          "HCloud::#{self.class.name.demodulize}".constantize
+        end
       end
     end
   end
--- lib/kozo/providers/hcloud/resources/ssh_key.rb
+++ lib/kozo/providers/hcloud/resources/ssh_key.rb
@@ -9,40 +9,6 @@ module Kozo
 
           attribute :name
           attribute :public_key
-
-          protected
-
-          def refresh
-            ssh_key = ::HCloud::SSHKey.find(id)
-
-            attribute_names
-              .excluding(:id)
-              .each { |attr| send(:"#{attr}=", ssh_key.send(attr)) }
-          end
-
-          def create
-            ssh_key = ::HCloud::SSHKey.new(**attributes.except(:id))
-            ssh_key.create
-
-            attribute_names
-              .each { |attr| send(:"#{attr}=", ssh_key.send(attr)) }
-          end
-
-          def update
-            ssh_key = ::HCloud::SSHKey.find(id)
-            ssh_key.name = name
-            ssh_key.public_key = public_key
-            ssh_key.update
-
-            attribute_names
-              .excluding(:id)
-              .each { |attr| send(:"#{attr}=", ssh_key.send(attr)) }
-          end
-
-          def destroy
-            ssh_key = ::HCloud::SSHKey.find(id)
-            ssh_key.delete
-          end
         end
       end
     end
