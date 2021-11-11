f75cf761fa5db929c00d1ba262efe66830e2cbe7
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-11-11 10:38:51 +0100

    Set correct arguments/attributes on SSH key resource

--- lib/kozo/providers/hcloud/resources/ssh_key.rb
+++ lib/kozo/providers/hcloud/resources/ssh_key.rb
@@ -7,8 +7,17 @@ module Kozo
         class SSHKey < Resource
           self.resource_name = "hcloud_ssh_key"
 
+          argument :name
+          argument :public_key
+          argument :labels, multiple: true
+
           attribute :name
           attribute :public_key
+
+          attribute :fingerprint
+
+          attribute :created
+          attribute :labels, multiple: true
         end
       end
     end
--- spec/kozo/providers/hcloud/resources/ssh_key_spec.rb
+++ spec/kozo/providers/hcloud/resources/ssh_key_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe Kozo::Providers::HCloud::Resources::SSHKey do
 
   before { stub_const("HCloud::SSHKey", ssh_key_class) }
 
-  it { is_expected.to have_attribute :name, :public_key }
+  it { is_expected.to have_arguments :name, :public_key, :labels }
+  it { is_expected.to have_attributes :name, :public_key, :fingerprint, :labels, :created }
 
   it "has a name" do
     expect(described_class.resource_name).to eq "hcloud_ssh_key"
