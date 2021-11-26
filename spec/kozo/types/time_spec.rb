e82a55aa60627b8e4a48ad5e3d0e622657fa8528
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-11-26 19:29:10 +0100

    Fix timezone in time spec

--- spec/kozo/types/time_spec.rb
+++ spec/kozo/types/time_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Kozo::Types::Time do
 
     it "casts a time value" do
       expect(described_class.cast("2000-01-01")).to eq Time.new(2000, 1, 1)
-      expect(described_class.cast("2000-01-01T12:00:00+01:00")).to eq Time.new(2000, 1, 1, 12)
+      expect(described_class.cast("2000-01-01T12:00:00Z")).to eq Time.new(2000, 1, 1, 12).in_time_zone("UTC")
     end
 
     it "does not cast invalid values" do
@@ -24,7 +24,7 @@ RSpec.describe Kozo::Types::Time do
     end
 
     it "serializes times correctly" do
-      expect(described_class.serialize(Time.new(2000, 1, 1, 12))).to eq "2000-01-01T12:00:00+01:00"
+      expect(described_class.serialize(Time.new(2000, 1, 1, 12).in_time_zone("UTC"))).to eq "2000-01-01T12:00:00Z"
     end
   end
 end
