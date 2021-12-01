5da2bc38a83f2eeae910ce7359a5bf99a4960d5b
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-12-01 20:23:35 +0100

    Ignore file matches in .kzignore

--- lib/kozo/parser.rb
+++ lib/kozo/parser.rb
@@ -14,9 +14,22 @@ module Kozo
 
       Dir[File.join(directory, "main.kz"), File.join(directory, "**", "*.kz")]
         .uniq
+        .reject { |file| ignores.any? { |ignore| file.include?(ignore) } }
         .each { |file| dsl.instance_eval(File.read(file)) }
 
       configuration
+    end
+
+    private
+
+    def ignores
+      @ignores ||= begin
+        File
+          .readlines(File.join(directory, ".kzignore"))
+          .map(&:chomp)
+      rescue Errno::ENOENT
+        []
+      end
     end
   end
 end
