13c42ea69dfd7912ed3f4c7b914e156e78a9c700
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-12-12 18:33:02 +0100

    Add test case for empty hash serialization

--- spec/core_ext/hash_spec.rb
+++ spec/core_ext/hash_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe CoreExt::Hash do
   subject(:hash) { { foo: "bar", baz: false } }
 
   describe "#as_s" do
+    context "when the hash is empty" do
+      subject(:hash) { {} }
+
+      it "serializes correctly" do
+        expect(hash.as_s).to eq "{}"
+      end
+    end
+
     it "serializes correctly" do
       expect(hash.as_s).to eq "{
   foo = \"bar\",
